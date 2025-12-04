dc69b747349a9df2fce1a0c309a001aec5a94ffa
neeti-yaav <neetiy32@gmail.com>
2025-12-04 23:21:13 +0530

    4Dec2025:23:21

---
+++ Basics.js
@@ -0,0 +1,11 @@
+// let a = 2;
+// let b = 5;
+// console.log("a+b =", a+b);
+
+
+let a = 2;
+let b = 5;
+console.log("a*b =", a*b);
+
+
+
--- index.js
+++ index.js
@@ -17,75 +17,79 @@
 
 
 
-let hello = "neetiYadav";
-console.log(hello);
+// let hello = "neetiYadav";
+// console.log(hello);
 
-let a,b;
-console.log(a,b);
+// let a,b;
+// console.log(a,b);
 
-let c=null;
-console.log(c);
+// let c=null;
+// console.log(c);
 
-let d= "neeti"
- d = 7;
- console.log(d);
+// let d= "neeti"
+//  d = 7;
+//  console.log(d);
 
 
- let person = {
-    firstName : "Milee",
-    age : 19
- }
- console.log(person.age, person.firstName);
- console.log(person["age"],person["firstName"]);
- let color
- let fruit={
-    color:"red",
-    "name":"apple",
-    "shape":"round",
-    weight:"1kg",
-    "taste":"tasty"
- }
+//  let person = {
+//     firstName : "Milee",
+//     age : 19
+//  }
+//  console.log(person.age, person.firstName);
+//  console.log(person["age"],person["firstName"]);
+//  let color
+//  let fruit={
+//     color:"red",
+//     "name":"apple",
+//     "shape":"round",
+//     weight:"1kg",
+//     "taste":"tasty"
+//  }
  
-console.log(fruit.weight,color);
+// console.log(fruit.weight,color);
 
-let vegetable = {
-    size:"long",
-    shape:"rectangle",
-    color:"green",
-    weight:"500gm",
-}
-console.log(vegetable.color, vegetable.weight);
-
-
-let vehicles = {
-    color:"pink",
-    size:"big",
-    price:"500K",
-    weight:"200000kg",
-
-}
+// let vegetable = {
+//     size:"long",
+//     shape:"rectangle",
+//     color:"green",
+//     weight:"500gm",
+// }
+// console.log(vegetable.color, vegetable.weight);
 
 
+// let vehicles = {
+//     color:"pink",
+//     size:"big",
+//     price:"500K",
+//     weight:"200000kg",
+
+// }
 
 
-console.log("the color of", vehicles.color ,"and the size is", vehicles.size, "and the price is", vehicles.price);
 
 
-let food = {
-    type:"chinese",
-    taste:"spicy",
-    price:"80rs",
-    location:"limbodi",
-    option:"pasta",
-}
-console.log(food.type, food.taste, food.price, food.location, food.option);
+// console.log("the color of", vehicles.color ,"and the size is", vehicles.size, "and the price is", vehicles.price);
 
 
-let student = {
-    rollno:128,
-    name:"Neeti",
-    branch:"CSE",
-    semester:"V",
-}
+// let food = {
+//     type:"chinese",
+//     taste:"spicy",
+//     price:"80rs",
+//     location:"limbodi",
+//     option:"pasta",
+// }
+// console.log(food.type, food.taste, food.price, food.location, food.option);
 
-console.log(student.name);
+
+// let student = {
+//     rollno:128,
+//     name:"Neeti",
+//     branch:"CSE",
+//     semester:"V",
+// }
+
+// console.log(student.name);
+
+
+let arr = ["milee","lalo","neeti","mitali"];
+console.log(arr[3],"and",arr[0]);

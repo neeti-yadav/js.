82fb5d041ff58ee19901540017b4a54f0dfea7ab
neeti-yaav <neetiy32@gmail.com>
2025-12-07 23:28:18 +0530

    7Dec2025:23:28

--- logical_op.js
+++ logical_op.js
@@ -1,9 +1,14 @@
-let name = "Milee";
-let age = 9;
-if(name==="Milee"&& age===19){
-    console.log("yes");
+// let name = "Milee";
+// let age = 9;
+// if(name==="Milee"&& age===19){
+//     console.log("yes");
     
-}else{
-    console.log("no");
+// }else{
+//     console.log("no");
     
+// }
+
+if(true && 451){    console.log("not gobba ");    
 }
+
+

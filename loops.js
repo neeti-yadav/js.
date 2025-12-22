3fd775fc8a49de1e752897f7b6643e2ee90d92ef
neeti-yaav <neetiy32@gmail.com>
2025-12-22 23:00:09 +0530

    22Dec2025:22:59

--- loops.js
+++ loops.js
@@ -5,3 +5,31 @@ console.log("-------------------------------");
 for(i=0;i<=8;i++){
     console.log(i);
 }
+
+for(i=0; i<5; i++){
+    console.log("Gabbar" + i);
+}
+
+
+
+console.log("-------------------------------");
+console.log("While Loop");
+console.log("-------------------------------");
+
+let j=1;
+while(j<6){
+    console.log(j);
+    j++;
+}
+
+
+
+console.log("-------------------------------");
+console.log("Do While Loop");
+console.log("-------------------------------");
+
+let k=1;
+do{
+    console.log(k);
+    k++;
+}while(k<7);

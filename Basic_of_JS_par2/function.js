70e5e17665917857808df35c70a2f32fbbe40080
neeti-yaav <neetiy32@gmail.com>
2025-12-26 22:20:21 +0530

    26Dec2025:22:20

--- Basic_of_JS_par2/function.js
+++ Basic_of_JS_par2/function.js
@@ -1,87 +1,134 @@
-console.log("-------------------------------");
-console.log("functions");
-console.log("-------------------------------");
-//  Method-1
+// console.log("-------------------------------");
+// console.log("functions");
+// console.log("-------------------------------");
+// //  Method-1
 
-let Rectangle={
-    length:2,
-    breadth:4
-}
+// let Rectangle={
+//     length:2,
+//     breadth:4
+// }
 
-function areaOfParameter (a,b){
+// function areaOfParameter (a,b){
 
-// console.log("area of parameter =>",2*(a+b));
-}
-areaOfParameter(Rectangle.length,Rectangle.breadth)
+// // console.log("area of parameter =>",2*(a+b));
+// }
+// areaOfParameter(Rectangle.length,Rectangle.breadth)
 
-function areaOfRectangle (x,y){
-    // console.log("Area of Rectangle is =",x*y);
-}
-// areaOfRectangle(Rectangle.length,Rectangle.breadth)
-
-
-let cone = {
-    length:5,
-    radius:8,
-} 
-
-function curvedSurfaceArea (l,r){
-    // console.log("The curved surface area of a cone is => ", 3.14*l*r);
-}
-curvedSurfaceArea(cone.length,cone.radius)
-
-console.log("-------------------------------");
-console.log("functions-Method-2 (arrow function)");
-console.log("-------------------------------");
-
-let sum=(a,b)=>{
-    // console.log(" sum is a+b",a+b);
-}
-sum(2,3)
-
-let areaOfCircle=(r)=>{
-    // console.log("area of circle",3.14*r*r)
-}
-areaOfCircle(3)
+// function areaOfRectangle (x,y){
+//     // console.log("Area of Rectangle is =",x*y);
+// }
+// // areaOfRectangle(Rectangle.length,Rectangle.breadth)
 
 
+// let cone = {
+//     length:5,
+//     radius:8,
+// } 
+
+// function curvedSurfaceArea (l,r){
+//     // console.log("The curved surface area of a cone is => ", 3.14*l*r);
+// }
+// curvedSurfaceArea(cone.length,cone.radius)
+
+// console.log("-------------------------------");
+// console.log("functions-Method-2 (arrow function)");
+// console.log("-------------------------------");
+
+// let sum=(a,b)=>{
+//     // console.log(" sum is a+b",a+b);
+// }
+// sum(2,3)
+
+// let areaOfCircle=(r)=>{
+//     // console.log("area of circle",3.14*r*r)
+// }
+// areaOfCircle(3)
 
 
-let areaOfRec=(l,b)=>{
-    // console.log("area of  rec = " ,l*b);
-
-}
-areaOfRec(Rectangle.length,Rectangle.breadth)
 
 
-areaOfPara=(n,m)=>{
-    // console.log("area of para =" ,2*(n+m));
-}
-areaOfPara(Rectangle.length,Rectangle.breadth)
+// let areaOfRec=(l,b)=>{
+//     // console.log("area of  rec = " ,l*b);
 
-console.log("-------------------------------");
-console.log("functions-Method-3 (return type)");
-console.log("-------------------------------");
-
-let returningTypeFunction=function (a,b){
-
-    return a+b
-}
- let value= returningTypeFunction(2,1)
-
- console.log("values is => ", value);
+// }
+// areaOfRec(Rectangle.length,Rectangle.breadth)
 
 
- let stringReturn=function (str) {
-    return str;
+// areaOfPara=(n,m)=>{
+//     // console.log("area of para =" ,2*(n+m));
+// }
+// areaOfPara(Rectangle.length,Rectangle.breadth)
+
+// console.log("-------------------------------");
+// console.log("functions-Method-3 (return type)");
+// console.log("-------------------------------");
+
+// let returningTypeFunction=function (a,b){
+
+//     return a+b
+// }
+//  let value= returningTypeFunction(2,1)
+
+//  console.log("values is => ", value);
+
+
+//  let stringReturn=function (str) {
+//     return str;
     
- }
- let strValue=stringReturn("NeetiYadav")
- console.log("value of string is => ", strValue);
+//  }
+//  let strValue=stringReturn("NeetiYadav")
+//  console.log("value of string is => ", strValue);
 
 
- let numberReturn=function(number){
-    return number;
- }
- let numberValue=numberReturn("21")
- console.log("number is = ", numberValue);
+//  let numberReturn=function(number){
+//     return number;
+//  }
+//  let numberValue=numberReturn("21")
+//  console.log("number is = ", numberValue);
+
+
+
+
+
+// let a=5,b=5
+
+// function sum(a,b){
+//     console.log("the sum is ",a+b);
+// }
+// sum(a,b)
+
+
+// function multiplication(a,b){
+//     console.log("the multiplication is",a*b);
+// }
+// multiplication(a,b)
+
+
+
+
+// function divide(e,f){
+//     console.log("the divide is ",e/f);
+// }
+// divide(a,b)
+
+
+
+// let sum=(a,b)=>{
+//     console.log("sum",a+b);
+// }
+// sum(a,b)
+
+
+
+// let multiply=(a,b)=>{
+//     console.log("multiply",a*b);
+// }
+// multiply(a,b)
+
+
+// let divide=(a,b)=>{
+//     console.log("divide",a/b);
+
+// }
+// divide(a,b)
+
--- Basics.js
+++ Basics.js
@@ -7,3 +7,27 @@ let a = 2;
 let b = 5;
 console.log("a*b =", a*b);
 
+
+let cat = {
+    legs:4,
+    tail:1,
+    teeth:2,
+    color:"white"
+}
+console.log(cat.legs,cat.tail,cat.teeth,cat.color)
+
+
+let juice={
+    flavour:"orange",
+    ml:500,
+    brand:"fanta"
+}
+console.log(juice.brand,juice.flavour,juice.ml);
+
+
+let sunscreen={
+    brand1:"dot&key",
+    brand2:"neutrogena",
+    brand3:"aqualogical"
+}
+console.log(sunscreen.brand1,sunscreen.brand2,sunscreen.brand3);
